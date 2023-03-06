2803920036624526b6a3a1125d6000f192d29cc0
kimsm23236 <kimsm23236@gmail.com>
2023-03-06 17:26:41 +0900

    [SuminKim][feat] -> TileMap, Terrain ocean,plain, random Set Ocean

---
+++ PathFinder/Assets/01.UnityProject/Scripts/Global/RDefine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RDefine
+{
+    public const string TERRAIN_PREF_OCEAN = "Terrain_Ocean";
+    public const string TERRAIN_PREF_PLAIN = "Terrain_Plain";
+
+    public const string OBSTACLE_PREF_PLAIN_CASTLE = "asdasd";
+    public enum TileStatusColor
+    {
+        DEFAULT, SELECTED, SEARCH, INACTIVE
+    }
+}
+    
---
+++ PathFinder/Assets/01.UnityProject/Scripts/PlayScene/TerrainController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour
+{
+    private const string TILE_FRONT_RENDERER_OBJ_NAME = "FrontRenderer";
+
+    private TerrainType terrainType = TerrainType.NONE;
+    public bool IsPassable { get; private set; } = false;
+    public int TileIdx1D { get; private set; } = -1;
+    public Vector2Int TileIdx2D { get; private set; } = default;
+    #region 길찾기 알고리즘을 위한 변수
+    private SpriteRenderer frontRenderer = default;
+    private Color defaultColor = default;
+    private Color selectedColor = default;
+    private Color searchColor = default;
+    private Color inactiveColor = default;
+
+    #endregion // 길찾기 알고리즘을 위한 변수
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+}   // class TerrainController
